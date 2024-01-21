9d75a872a1dfd223ded36863b6c71ca0384d1747
SantiagoOscarVera <santiagovera280@gmail.com>
2024-01-21 01:24:51 -0300

    php

--- hello-world.php
+++ hello-world.php
@@ -37,5 +37,20 @@ echo my_constant . "\n";
 
 $mi_array = [$bool, $numero, $double];
 echo gettype($mi_array) . "\n";
-echo $mi_array[1];
-echo $mi_array[3];
+echo $mi_array[1] . "\n";
+array_push($mi_array,$variable1); /* agregamos un valor mass a la lista */
+print_r ($mi_array) . "\n"; /* imprimimos el array o lista */
+
+/* diccionario */
+
+$mi_dict = array("string" => $variable1, "int" => $numero, "bool" => $bool);
+echo gettype($mi_dict) . "\n";
+echo $mi_dict["int"] . "\n";
+
+/* set */
+array_push($mi_array,"Brais");
+array_push($mi_array,"Brais");
+print_r($mi_array);
+print_r(array_unique($mi_array)); /* te deja unicamente valores unicos */
+
+/* flujos */
